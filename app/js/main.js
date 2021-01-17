aaeb73c8b44e29bd9def5f41479e489700aaacd2
HelLuv <works.person@gmail.com>
2021-01-17 23:16:35 +0300

    added products and products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function () {
+	$(".rate-star").rateYo({
+		rating: 5.0,
+		starWidth: "12px",
+		ratedFill: "#ffa726",
+		readOnly: true
+	});
 
+	$('.product-slider__inner').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+
+	var mixer = mixitup('.products__inner-box');
 });
