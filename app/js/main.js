fb4c01cf59dd126ed662b45a3658a567cf4f266f
HelLuv <works.person@gmail.com>
2021-01-20 15:23:45 +0300

    added new-item, page, adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-	$(".rate-star").rateYo({
-		rating: 5.0,
-		starWidth: "12px",
-		ratedFill: "#ffa726",
-		readOnly: true
-	});
+	// $(".rate-star").rateYo({
+	// 	rating: 5.0,
+	// 	starWidth: "12px",
+	// 	ratedFill: "#ffa726",
+	// 	readOnly: true
+	// });
 
 	$('.product-slider__inner').slick({
 		dots: true,
@@ -12,6 +12,34 @@ $(function () {
 		infinite: true,
 		slidesToShow: 4,
 		slidesToScroll: 4
+	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+	$('.icon-th-list').on('click', function () {
+		$('.product__item').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.icon-th-large').removeClass('active');
+	});
+	$('.icon-th-large').on('click', function () {
+		$('.product__item').removeClass('list');
+		$('.icon-th-large').addClass('active');
+		$('.icon-th-list').removeClass('active');
+	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	});
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
 	});
 
 	var mixer = mixitup('.products__inner-box');
